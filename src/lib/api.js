6228941df8d48da03401119905f10b871373cd25
endukuraa <endukuraa11@gmail.com>
2026-01-21 14:56:00 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "backen-production-be04.up.railway.app";
+const API_BASE_URL = "https://backen-production-be04.up.railway.app";
 
 export const registerPerson = async (data, photos) => {
   const formData = new FormData();
