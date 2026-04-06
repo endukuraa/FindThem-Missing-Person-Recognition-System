7f75db85dc3b55a899f8d15c064aab990e63a0e6
endukuraa <endukuraa11@gmail.com>
2026-04-06 10:39:41 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://endukuraa-production.up.railway.app";
+const API_BASE_URL = "https://backend-production-cf60.up.railway.app";
 
 export const registerPerson = async (data, photos) => {
   const formData = new FormData();
